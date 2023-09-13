328213ac271ba79872336942a13f05a9cd278e36
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2023-09-13 20:47:55 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-""" adds all arguments to a Python list, and then save them to a file"""
-
-
-import json
+"""
+File ``7-add_item.py``
+Adds all arguments to a Python list, and then save them to a file
+"""
+from pathlib import Path
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import json
 
-list1 = []
-try:
-    list1 = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    list1 = []
-for arg in sys.argv[1:]:
-    list1.append(arg)
-save_to_json_file(list1, "add_item.json")
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = []
+if Path('add_item.json').exists():
+    args = load_from_json_file('add_item.json')
+
+for i in range(1, len(sys.argv)):
+    args.append(str(sys.argv[i]))
+
+save_to_json_file(args, 'add_item.json')
