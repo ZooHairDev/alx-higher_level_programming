5869adde280debd3119f050c34d995372003a4bf
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2023-09-13 20:58:43 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""
-This script adds all arguemnts to a python list and then saves them to a file
-"""
-import json
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""Are you docuemnted ?"""
 
 
-arg_list = []
-if __name__ == '__main__':
-    for argument in argv:
-        # check if it's the first argument
-        if argv.index(argument) == 0:
-            continue
-        # add other arguments to a list
-        arg_list.append(argument)
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
     try:
-        # load existing list
-        new_list = load_from_json_file("add_item.json")
-        # add new list
-        new_list += arg_list
-        # save newly created list
-        save_to_json_file(new_list, "add_item.json")
+        loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        # if there's no exissting list create it
-        save_to_json_file(arg_list, "add_item.json")
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
