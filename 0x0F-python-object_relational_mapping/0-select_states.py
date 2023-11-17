0cd26e077281933ad02c7c0f816a2c5b86307d90
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2023-11-17 12:52:23 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb as mysql
-
-    try:
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-    except Exception:
-        print('Failed to connect to the database')
-        exit(0)
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-
-    result_query = cursor.fetchall()
-
-    for row in result_query:
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id ASC""")
+    result = c.fetchall()
+    for row in result:
         print(row)
-
-    cursor.close()
-    db.close()
