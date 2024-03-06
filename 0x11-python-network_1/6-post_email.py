a7fdbd9a9e534bd2ad9cfebf7c9a7cceb564ff45
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2024-03-06 18:55:07 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response."""
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
+"""
 import sys
 import requests
 
